9654d22d2b1a5bea7ce11ef2ab83d9bd1f8796e8
Lam <lamkyo373@gmail.com>
2025-03-27 14:17:28 +0700

    added gitignore file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import rootRouter from "./routers/root.router";
 import { handleError } from "./common/helpers/error.helper";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use(rootRouter);
 app.use(handleError);
 
