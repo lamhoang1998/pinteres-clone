adec0754ba9d609dde6fa62895f2ecde80677b72
Lam <lamkyo373@gmail.com>
2025-03-31 21:57:14 +0700

    fix bugs in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,9 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+	res.sendFile(
+		path.join(__dirname, "../../frontend/dist", "../../frontend/index.html")
+	);
 });
 
 app.use(rootRouter);
