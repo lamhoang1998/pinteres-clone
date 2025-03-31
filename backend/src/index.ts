5c5f95fa8d271ffb14c9101f32bf9af5182852ce
Lam <lamkyo373@gmail.com>
2025-03-31 21:47:02 +0700

    fix the routing error in render in index.ts in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,11 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+});
+
 app.use(rootRouter);
 app.use(handleError);
 
